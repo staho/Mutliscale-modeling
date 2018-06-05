1821e03f9fd8f3cd7c5f029248ffbbaf8d6bcabf
Kamil Stachowicz <stacho688@gmail.com>
2018-06-05 23:33:45 +0200

    Von Neumann progress

--- JavaFx/src/Model/NeighbourHood/VonNeumannNeighbourhood.kt
+++ JavaFx/src/Model/NeighbourHood/VonNeumannNeighbourhood.kt
@@ -11,38 +11,16 @@ class VonNeumann(nXCells: Int, nYCells: Int, var periodic: Boolean = true): Neig
     override fun computeIndexes(i: Int, j: Int): List<IndexPoint> {
         val indexes: MutableList<IndexPoint> = mutableListOf()
 
-        when (i) {
-            0 -> {
-                indexes.add(IndexPoint(lastX, j))
-                indexes.add(IndexPoint(i + 1, j))
-            }
-            lastX -> {
-                indexes.add(IndexPoint(0, j))
-                indexes.add(IndexPoint(i - 1, j))
-            }
-            else -> {
-                indexes.add(IndexPoint(i - 1, j))
-                indexes.add(IndexPoint(i + 1, j))
-
+        if(i in 1 until lastX && j in 1 until lastY) {
+            for(iterX in i-1..i+1){
+                for(iterY in j-1..j+1){
+                    if(iterX != i && iterY != j) {
+                        indexes.add(IndexPoint(iterX, iterY))
+                    }
+                }
             }
         }
-
-        when (j) {
-            0 -> {
-                indexes.add(IndexPoint(i, lastY))
-                indexes.add(IndexPoint(i, j + 1))
-            }
-            lastY -> {
-                indexes.add(IndexPoint(i, 0))
-                indexes.add(IndexPoint(i, j - 1))
-            }
-            else -> {
-                indexes.add(IndexPoint(i, j - 1))
-                indexes.add(IndexPoint(i, j + 1))
-
-            }
-        }
-
+        
         return indexes
     }
 }
